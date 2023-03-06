13343c4f57efcd8cbe792cc340a9ae6759e94510
AlexanderGhosh <ghoshalexander@gmail.com>
2023-03-06 12:51:16 +0000

    isolates red pixels

--- main.py
+++ main.py
@@ -2,8 +2,38 @@ import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as graph
 
-im = cv.imread('./dataset/1.jpg')
-im = cv.cvtColor(im, cv.COLOR_BGR2RGB)
 
-graph.imshow(im)
-graph.show()
+def how_red(orig: tuple) -> float:
+    b1, g1, r1 = orig
+    t = (r1 + (1.0 - g1) + (1.0 - b1)) * 0.333
+    return t
+
+
+im = cv.imread('./dataset/1.jpg')
+
+mask = np.zeros((im.shape[:-1]), dtype=np.uint8)
+
+THRESH_HOLD = 0.7
+for x in range(im.shape[0]):
+    for y in range(im.shape[1]):
+        close = how_red(im[x, y])
+        if close >= THRESH_HOLD:
+            px = 255
+        else:
+            px = 0
+        mask[x, y] = px
+
+
+cv.imshow('image', im)
+cv.startWindowThread()
+cv.imshow('mask', mask)
+
+cv.waitKey(0)
+
+cv.destroyAllWindows()
+cv.waitKey(1)
+cv.waitKey(1)
+cv.waitKey(1)
+cv.waitKey(1)
+
+
