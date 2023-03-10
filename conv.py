311480f7b3baab8c3ecf9319190483770e8d9a2a
AlexanderGhosh <ghoshalexander@gmail.com>
2023-03-10 17:07:52 +0000

    works with dataset but very slow for the full video

--- conv.py
+++ conv.py
@@ -1,10 +1,29 @@
 import matplotlib.pyplot as plot
 import torch
-import torchvision.io
+from torch.utils.data import Dataset, DataLoader
+from torchvision.io import read_video, write_video, read_video_timestamps
 from torch import nn
 import numpy as np
 import cv2 as cv
 from Timer import Timer
+
+
+class VideoDataset(Dataset):
+    def __init__(self, file, max):
+        self.loc = file
+        meta = read_video_timestamps(file)
+        self.frame_count = min(max, len(meta[0]))
+        self.fps = meta[1]
+        data = read_video(self.loc, 0, 0)[0]
+        self.shape = (self.frame_count, *data.shape[1:])
+
+    def __len__(self):
+        return self.frame_count
+
+    # item, label
+    def __getitem__(self, item):
+        data = read_video(self.loc, item, item, output_format='TCHW')
+        return data[0][0], 0
 
 
 class Threshold(nn.Module):
@@ -23,7 +42,6 @@ class ToColour(nn.Module):
     def forward(self, x):
         x_ = torch.swapaxes(x, 1, -1) * 255.0
         x_ = x_.repeat(1, 1, 1, 3)
-        print(x_.shape)
         return x_
 
 
@@ -36,10 +54,11 @@ class Normalize(nn.Module):
         t = torch.div(x, n)
         return t
 
+
 timer = Timer('Main Timer')
 timer.start()
 MAX_FRAMES = 100
-TARGET_COLOUR = [1.0, 0.0, 0.0]
+TARGET_COLOUR = [0.0, 0.0, 1.0]
 
 device = None
 if torch.cuda.is_available():
@@ -51,13 +70,13 @@ else:
 
 # im = cv.imread('./dataset/1.jpg')
 # im = cv.cvtColor(im, cv.COLOR_BGR2RGB).astype(np.float32)
-with Timer('Loading video into memory'):
+'''with Timer('Loading video into memory'):
     # video = cv.VideoCapture('./dataset/3.mp4')
     # frame count, channels, with , height
     # data = np.zeros([int(t) for t in (min(MAX_FRAMES, video.get(7)), 3, video.get(3), video.get(4))])
-    data = torchvision.io.read_video('./dataset/3.mp4', output_format='TCHW', end_pts=MAX_FRAMES-1)
+    data = read_video('./dataset/3.mp4', output_format='TCHW', end_pts=MAX_FRAMES-1)
     data = data[0].to(torch.float)
-    ''' i = 0
+    '' i = 0
     while video.isOpened() and i < MAX_FRAMES:
         suc, frame = video.read()
         if not suc:
@@ -71,12 +90,12 @@ with Timer('Loading video into memory'):
 
         i += 1'''
 
-    # video.release()
+# video.release()
 
-    '''data = data.astype(np.float32)
-    x = torch.from_numpy(data)'''
+'''data = data.astype(np.float32)
+    x = torch.from_numpy(data)''
     x = data.to(device)
-
+'''
 with Timer('Convolution weight creation'):
     w_ = np.asarray([TARGET_COLOUR])
     w_ = np.expand_dims(w_, axis=-1)
@@ -95,13 +114,22 @@ with Timer('Model creation'):
     model = nn.Sequential(Normalize(), conv, thresh, ToColour())
     model.to(device)
 
+
 with Timer('Run model'):
-    y = model(x)
-    y = y.detach().cpu()
+    d_ = VideoDataset('./dataset/3.mp4', MAX_FRAMES)
+    video_loader = DataLoader(d_, batch_size=1, shuffle=False)
+    result = torch.ones(d_.shape)
+    i = 0
+    for d, _ in video_loader:
+        x = d.to(device)
+        y = model(x)
+        y = y.detach().cpu()
+        result[i] = torch.transpose(y, 1, 2)
+        i += 1
 
 
 with Timer('Save data'):
-    torchvision.io.write_video('002.mov', y, 25)
+    write_video('002.mov', result, 25)
     '''shape_ = y.shape[2:]
     fourcc = cv.VideoWriter_fourcc(*'DIVX')
     writer = cv.VideoWriter('002.mov', fourcc, 25.0, shape_, isColor=False)
