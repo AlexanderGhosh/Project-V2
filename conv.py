e82502baa4808b5386ae1fc2e3d18ea52de1e500
AlexanderGhosh <ghoshalexander@gmail.com>
2023-03-16 14:38:52 +0000

    uses the same func as in main for colour similarity

--- conv.py
+++ conv.py
@@ -20,6 +20,7 @@ class VideoDataset2(Dataset):
         if os.path.isdir(path):
             self.len = len(os.listdir(path))
             img = read_image(f'{self.path}/00001.jpg')
+            img = img.permute(1, 2, 0)
             self.shape = self.len, *img.shape
 
     def __len__(self):
@@ -27,7 +28,7 @@ class VideoDataset2(Dataset):
 
     def __getitem__(self, item):
         file = f'{self.path}/{item + 1:05d}.jpg'
-        return read_image(file), item
+        return read_image(file).permute(1, 2, 0), item
 
 
 class VideoDataset(Dataset):
@@ -63,28 +64,22 @@ class ToColour(nn.Module):
         self.activation = nn.ReLU()
 
     def forward(self, x):
-        # x = self.activation(x[:, 0, :, :] + x[:, 1, :, :])
-        x = x[:, 0, :, :]
         x = x * 255.0
-        x = x.repeat(1, 3, 1, 1)[0]
+        x = x.repeat(1, 1, 1, 3)
         return x
 
 
 class Similar(nn.Module):
     def __init__(self, target):
         super().__init__()
-        self.target = target
+        self.loss = nn.MSELoss(reduction='none')
+        self.target = torch.asarray(target).to(torch.float).to(device)
 
     def forward(self, x):
-        return torch.abs(1.0 - (self.target - x))
-
-
-def norm(x):
-    i = x[0] + 256.0 * (x[1] + x[2] * 256.0)
-    i *= INV_COLOUR
-    i *= INV_COLOUR
-    i *= INV_COLOUR
-    return i
+        t = self.loss(x, self.target)
+        t = torch.sum(t, dim=-1)[:, :, :, None]
+        t = torch.div(t, 3.0 ** 0.5)
+        return 1.0 - t
 
 
 class Normalize(nn.Module):
@@ -92,21 +87,14 @@ class Normalize(nn.Module):
         super().__init__()
 
     def forward(self, x):
-        i = x[:, 0, :, :] + 256.0 * (x[:, 1, :, :] + x[:, 2, :, :] * 256.0)
-        i *= INV_COLOUR
-        i *= INV_COLOUR
-        i *= INV_COLOUR
-        i = i[:, None, :, :]
-        # n = torch.sum(x, dim=1)[:, None, :, :]
-        t = torch.pow(x, 0)
-        t = torch.mul(t, i)[:, 0, :, :][:, None, :, :]
+        t = torch.div(x, 255.0)
         return t
 
 
 timer = Timer('Main Timer')
 timer.start()
 MAX_FRAMES = 100
-TARGET_COLOUR = [0, 0, 255]
+TARGET_COLOUR = [0, 0, 1]
 
 device = None
 if torch.cuda.is_available():
@@ -116,50 +104,9 @@ else:
     device = torch.device('cpu')
     print('cpu')
 
-# im = cv.imread('./dataset/1.jpg')
-# im = cv.cvtColor(im, cv.COLOR_BGR2RGB).astype(np.float32)
-'''with Timer('Loading video into memory'):
-    # video = cv.VideoCapture('./dataset/3.mp4')
-    # frame count, channels, with , height
-    # data = np.zeros([int(t) for t in (min(MAX_FRAMES, video.get(7)), 3, video.get(3), video.get(4))])
-    data = read_video('./dataset/3.mp4', output_format='TCHW', end_pts=MAX_FRAMES-1)
-    data = data[0].to(torch.float)
-    '' i = 0
-    while video.isOpened() and i < MAX_FRAMES:
-        suc, frame = video.read()
-        if not suc:
-            break
-        n = np.sum(frame, axis=2)
-
-        n = np.repeat(n[:, :, np.newaxis], 3, axis=2)
-        d = frame / n
-
-        data[i] = d.T
-
-        i += 1'''
-
-# video.release()
-
-'''data = data.astype(np.float32)
-    x = torch.from_numpy(data)''
-    x = data.to(device)
-'''
-with Timer('Convolution weight creation'):
-    w_ = np.asarray([TARGET_COLOUR, [1, 1, 0]])
-    w_ = np.expand_dims(w_, axis=-1)
-    w_ = np.expand_dims(w_, axis=-1)
-
-    w_ = torch.asarray(w_).to(torch.float)
-    w_ = nn.Parameter(w_)
 
 with Timer('Model creation'):
-    conv = nn.Conv2d(3, 2, 1)
-    conv.weight = w_
-    conv.bias = nn.Parameter(torch.zeros(conv.bias.shape))
-
-    thresh = Threshold(0.4)
-
-    model = nn.Sequential(Normalize(), Similar(norm(TARGET_COLOUR)), thresh, ToColour())
+    model = nn.Sequential(Normalize(), Similar(TARGET_COLOUR), Threshold(0.7), ToColour())
     model.to(device)
 
 with Timer('Run model'):
@@ -175,8 +122,6 @@ with Timer('Run model'):
         i += 1
 
 with Timer('Save data'):
-    result = torch.transpose(result, 1, -1)
-    result = torch.transpose(result, 1, 2)
     write_video('002.mov', result, 25)
     '''shape_ = y.shape[2:]
     fourcc = cv.VideoWriter_fourcc(*'DIVX')
